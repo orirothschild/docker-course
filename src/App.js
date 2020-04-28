81036d5360bf7614bee91659331effe4a5f27f7b
ori rothschild <rotchildori@gmail.com>
2020-04-28 10:03:42 -0400

    change shit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>hellwwwo</code> and save to reload.
+          Edit <code>hellwwwo</code> and save to reload feature.
         </p>
         <a
           className="App-link"
